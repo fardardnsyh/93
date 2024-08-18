d23d736bedfd8ad569b4f1920dd204d810d2b16e
Farid Ardiansyah <faridardiansyah061@gmail.com>
2024-08-18 15:04:33 +0700

    Files upload

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import FormGenerator from "./form-generator";
+import Header from "@/components/ui/Header";
+import { SessionProvider } from "next-auth/react";
+
+export default function Home() {
+  return (
+    <SessionProvider>
+      <Header />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <FormGenerator />
+      </main>
+    </SessionProvider>
+  );
+}
